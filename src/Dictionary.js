667effda711759672755762861248ca77550e506
SandraLima2813 <sandra.c.nicolau@gmail.com>
2021-05-01 19:05:29 +0200

    Added meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,34 @@
-import React, {useState} from "react"; 
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
-import axios from "axios"; 
 
-export default function Dictionary () {
-  let [keyword, setKeyword] =  useState ("");
-  
-  function handleResponse (response) {
-    console.log(response.data[0]);
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
   }
 
-  function search (event) {
-    event.preventdefault();
+  function search(event) {
+    event.preventDefault();
 
+    // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
-
-  function handleKeywordChange (event) {
-    setKeyword (event.target.value);
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   return (
-  <div className= "Dictionary">
-  <form onSubmit={search}>
-    <input type= "search"  onChange ={handleKeywordChange} />
-  </form>
-  
-  </div> 
-);
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
