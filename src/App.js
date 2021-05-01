8a7e6f4f6a1c96dea22935bd5ca9cacd9052f078
SandraLima2813 <sandra.c.nicolau@gmail.com>
2021-05-01 16:30:34 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className= "container">
       <header className="App-header">
-   <h1>Hello</h1>
+   <h1>Welcome to the Dictionary!</h1>
       </header>
+      <main> 
+        <Dictionary />
+      </main>
+      <footer>
+        <small>Coded by Sandra Lima
+        </small>
+        </footer>
+      </div>
     </div>
   );
 }
